d31380f3e5c5ce97aebbe91e56d1a61d98b77458
Bastien Crettenand <bastien.crettenand@hotmail.com>
2019-02-13 15:20:10 +0100

    Add static methods

--- src/slsql.php
+++ src/slsql.php
@@ -2,7 +2,7 @@
 /**
  * WTFPL License (http://www.wtfpl.net/) - https://gitlab.com/CrBast/php-sqlsimplelife/blob/master/LICENSE
  * 
- * Class that simply makes life easier
+ * SimpleLifeSQL
  * 
  * Default configuration : XAMPP config
  * IN (array([
@@ -24,15 +24,18 @@
  * Return Type array([value], [status], [message])
  */
 class slsql{
-    private $dsn, 
-        $user = 'root', 
-        $password = '', 
-        $db, $dbName, 
-        $isConnected = false;
+    private $dsn;
+    private $user;
+    private $password;
+    private $db;
+    private $dbType = "ihfgierb";
+    private $dbName;
+    private $isConnected = false;
 
     public function construct($params){
         $this->dbName = $params['dbName'];
-        $this->dsn = isset($params['host']) ? $params['host'] : '127.0.0.1:3366';
+        $this->dsn = isset($params['host']) ? $params['host'] : '127.0.0.1:33066';
+        $this->dbType = isset($params['dbType']) ? $params['dbType'] : 'mysql';
         $this->user = isset($params['user']) ? $params['user'] : 'root';
         $this->password = isset($params['psw']) ? $params['psw'] : '';
     }
@@ -45,13 +48,14 @@ class slsql{
      *      [message] = Exception message => if [status] = 0
      */
     public function connect(){
+        echo $this->dbName;
         try {
             $this->db = $this->createDB(); 
             $this->isConnected = true;
-            return $this->createMessage('', 1, '');
+            return createMessage('', 1, '');
         } catch ( Exception $e ) 
         {  
-            return $this->createMessage('', 0, $e->getMessage());
+            return createMessage('', 0, $e->getMessage());
         }
     }
 
@@ -59,7 +63,7 @@ class slsql{
         try{
             $this->db = $this->createDB();
         } catch( Exception $e ){
-
+            return createMessage('', 0, $e->getMessage());
         }
     }
 
@@ -84,23 +88,37 @@ class slsql{
         try {
             $stmt = $this->db->prepare($request); 
             $stmt->execute($array); 
-            return  $this->createMessage($stmt, 1, '');
+            return  createMessage($stmt->fetchAll(), 1, '');
         } catch (Exception $e) {
-            return  $this->createMessage('', 0, $e->getMessage());
+            return  createMessage('', 0, $e->getMessage());
         }
-    }
-
-    /**
-     * Status : 1 = OK | 0 = problem
-     */
-    private function createMessage($value, $status, $message){
-        return array('value' => $value, 'status' => $status, 'message' => $message);
     }
 
     /**
      * Create DB object (PDO)
      */
-    private function createDB(){
-        return new PDO('mysql:dbname='.$this->dbName . ';' . $this->dsn, $this->user, $this->password);
+    public function createDB(){
+        return new PDO($this->dbType.':dbname='.$this->dbName . ';host=' . $this->dsn, $this->user, $this->password);
     }
+
+    public static function go($request, $array){
+        try{
+            require '.env';
+        } catch (Exception $e) { exit(createMessage('', 0, 'Cannot find <.env> file')); }
+        try {
+            $db = new PDO($env['DBType'].':dbname='. $env['DBName'] .';host=' . $env['Host'], $env['User'], $env['Password']);
+            $stmt = $db->prepare($request); 
+            $stmt->execute($array); 
+            return createMessage($stmt->fetchAll(), 1, '');
+        } catch (Exception $e) {
+            return createMessage('', 0, $e->getMessage());
+        }
+    }
+}
+
+/**
+ * Status : 1 = OK | 0 = problem
+ */
+function createMessage($value, $status, $message){
+    return array('value' => $value, 'status' => $status, 'message' => $message);
 }
